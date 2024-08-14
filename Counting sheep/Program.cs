95f9936ff3c9cc747711df7dc8169bf5472c6200
cowboyHat123111 <cowboy.hat1@protonmail.com>
2024-08-14 17:25:52 +0300

    Update footer links

---
+++ Counting sheep/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Counting_sheep
+{
+    internal class Program
+    {
+        /*public static int CountSheeps(bool[] sheeps)
+        {
+            int counter = 0;
+
+            foreach (bool item in sheeps)
+            {
+                if (item)
+                {
+                    ++counter;
+                }
+            }
+            return counter;
+        }*/
+
+        public static int CountSheeps(bool[] sheeps)
+        {
+            return sheeps.Count(s => s is true);
+        }
+
+        static void Main(string[] args)
+        {
+            bool[] bools = { true, false, true, true, false, true };
+
+            Console.WriteLine(CountSheeps(bools));
+        }
+    }
+}
